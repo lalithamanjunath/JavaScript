edf0fa2be5ad103aa8d9bca7747502ca2491ec40
lalithamanjunath <lalithamanjunath05@gmail.com>
2025-12-07 20:04:09 +0530

    datatypes

---
+++ 02_variables.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs , not browser 
+
+console.log(3
+    +2
+) // This not a good practice, code readability should be high
+
+let name = "Lalitha" // string datatype
+let age = 19 // number-value
+let isLoggedIn = true //boolean datatype
+
+// premitive datatypes-->
+// number => 2 to power 53
+// bigint
+// null => standalone value aur null ek object hai
+// undefined => value not assigned
+// symbol => unique
+
+//object
+
+console.log(typeof "Lalitha" )
+
+console.log(typeof null )
