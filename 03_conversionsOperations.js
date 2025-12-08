7a6369c00420224ac7d644ffb6ad594034992ba7
lalithamanjunath <lalithamanjunath05@gmail.com>
2025-12-08 18:04:32 +0530

    Conversions of datatypes

---
+++ 03_conversionsOperations.js
@@ -0,0 +1,29 @@
+let score = 33
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueINNumber = Number(score) // explicit conversion
+//console.log(typeof valueINNumber);
+//console.log(valueINNumber);
+
+//"33" => 33 - ye convert hota hai
+//"33abc" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn); // object
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Lalitha" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);; // object
+
