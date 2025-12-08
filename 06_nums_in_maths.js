bce4e14c01c62b32705c3d83aecc0a2e3ff3a5f5
lalithamanjunath <lalithamanjunath05@gmail.com>
2025-12-08 22:40:29 +0530

    Numbers in Math

---
+++ 06_nums_in_maths.js
@@ -0,0 +1,10 @@
+const score = 100;
+
+const balance = new Number(50); // number object
+
+console.log(balance); // [Number: 50]
+
+console.log(balance.toString().length); // 2
+console.log(balance.toFixed(3)); 
+
+
