700a60cf15e3a55d1e8b8e80728c9f5143f9425a
lalithamanjunath <lalithamanjunath05@gmail.com>
2025-12-08 18:27:29 +0530

    Operations

--- 03_conversionsOperations.js
+++ 03_conversionsOperations.js
@@ -27,3 +27,38 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);; // object
 
+//  ****************** Operations ********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+console.log(2+2); // 4
+console.log(2-2); // 0
+console.log(2*2); // 4
+console.log(2/2); // 1 
+console.log(5%2); // 1 (remainder)
+console.log(5**2); // 25 (exponentiation) 5*5
+
+let str1 = "Hello "
+let str2 = "Lalitha"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // "12"
+// console.log(1 + "2"); // "12"
+// console.log("1" + 2 + 2); // "122"
+// console.log(1 + 2 + "2"); // "32"
+
+// console.log( 3 + 2 * 2 % 3 ); // 3 - Ye khafi exam ke liye important hai lekin ye code readability ke liye acha nahi hai
+
+console.log(+true); // 1
+console.log(+"");
+
+let num1, num2, num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;  // 101
+console.log(gameCounter);
+
